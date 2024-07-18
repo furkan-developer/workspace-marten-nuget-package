1ff540857da405dfc32e399b017e67950a88a385
furkan aydin <furknn2112@gmail.com>
2024-07-18 21:01:32 +0300

    chore: Use Marten nuget package for using PostgreSQL as document DB
    - Install Marten nuget package
    - Add connection string to appsettings.json file
    - Configure Marten nuget package
    - Create two endpoint for testing the API

--- SampleAPI/Program.cs
+++ SampleAPI/Program.cs
@@ -1,6 +1,34 @@
+using System.Formats.Asn1;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMarten(options =>{
+    options.Connection(builder.Configuration.GetConnectionString("PostgreSQL")!);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (IQuerySession querySession) => {
+    
+    var products =   await querySession.Query<Product>().ToListAsync();
+    
+    return Results.Ok(products);
+
+});
+
+app.MapGet("/create-product", async (IDocumentSession documentSession) => {
+
+    var product = new Product("Product-1",10);
+    documentSession.Store(product);
+    await documentSession.SaveChangesAsync();
+    
+    return Results.Ok(new {id = product.Id});
+});
 
 app.Run();
+
+
+public record Product(string ProductName, int quantity){
+    public Guid Id { get; set; }
+}
